f3e65b49d14f9dd4b338b75e8bef2ca57b7bb4d4
Orlando <orla.grnk@gmail.com>
2022-09-25 19:27:40 -0300

    armando carrito de compra

---
+++ Inventario.js
@@ -0,0 +1,8 @@
+let arrayProductos = [
+{
+    codigo: 1, 
+    nombre: `iPhone 7`,
+    precio: 70000,
+    descripcion: "Estado impecable"
+}
+]
--- archivo.js
+++ archivo.js
@@ -3,6 +3,45 @@ Consigna: Seleccionar una opcion de reparacion y devolver presupuesto.
 */
 
     //Reservar turnos y que me genere Listas de Turnos reservados
+    //Api de github.
+fetch('https://api.github.com/users/Orly-90')
+.then((resp) => resp.json())
+.then((data) => {
+  let avatar = document.getElementById("avatar");
+  avatar.innerHTML = `<img class="avatar" src="${data.avatar_url}">`;
+}).catch(error=> {
+      Swal.fire("error");
+  });
+
+    const fetchData = position => {
+        const {latitude, longitude} = position.coords;
+        fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => WeatherData(data))
+    }
+    const WeatherData = data => {
+        let temperatura = document.getElementById("temperatura");
+        temperatura.innerHTML = `La Temperatura en ${data.name} es de ${data.main.temp}°`
+        if(Number(data.main.temp) > 16) {
+            Swal.fire({
+                title: 'Lindo día para comprar productos de verano!',
+                showConfirmButton: false,
+                timer: 2500,
+                icon: 'success'
+        });
+        }else {
+          Swal.fire({
+            title: 'Que frío! A comprar productos de invierno!',
+            showConfirmButton: false,
+            timer: 2500,
+            icon: 'success'
+          });
+        };
+      };
+    
+    const onLoad = () => {
+        navigator.geolocation.getCurrentPosition(fetchData);
+    }
 
     class Reservacion
     {
@@ -41,7 +80,7 @@ Consigna: Seleccionar una opcion de reparacion y devolver presupuesto.
             entrar = true
         }
         if (entrar) {
-            Swal.fire({
+            Swal.fire({         //sweet Alert
                 position: 'center',
                 icon: 'error',
                 title: 'Oops...algo anda mal',
@@ -55,7 +94,7 @@ Consigna: Seleccionar una opcion de reparacion y devolver presupuesto.
             parrafo.innerHTML = "";             //creo los objetos con los datos capturados
             let nuevaReserva = new Reservacion(entradaNombre, entradaApellido, entradaCelu, entradaMail);
             arrayDeReservas.push(nuevaReserva); //genero los arrays
-            Swal.fire({
+            Swal.fire({                 //sweet Alert
                 position: 'center',
                 icon: 'success',
                 title: 'Tu reserva fue exitosa',
